9cd95c81af9b6540b9097bcedd62a8cc72c3867d
LAPTOP-NFAOORIB\JayRand <jayrand.dev@gmail.com>
2020-11-26 18:21:16 -0800

    trying to get a homepage setup

---
+++ src/pages/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+export default function Home({ data }) {
+  return(
+    <Layout>
+      <SEO title="home" />
+      <h1>My Wordpress Blog</h1>
+      <h4>Posts</h4>
+      {data.allWpPost.nodes.map((node) => (
+        <div key={node.id}>
+          <p>
+            <Link to={node.slug}>
+              {node.title}
+            </Link>
+          </p>
+          <div dangerouslySetInnerHTML={{__html: node.excerpt}}/>
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    allWpPost {
+      nodes {
+        id
+        title
+        excerpt
+        slug
+      }
+    }
+  }
+`
